c324c61092aa581d7852891221368c3f61689ac6
Syed Humza <105381562+syedhumzasalman@users.noreply.github.com>
2025-07-03 06:26:41 +0500

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -9,17 +9,6 @@ import {
     getDocs,
 } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-firestore.js";
 
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
-    apiKey: "AIzaSyCkJnxlncA3zMtLzvff4lzRrlLvWj8bbHw",
-    authDomain: "to-do-list-66f51.firebaseapp.com",
-    projectId: "to-do-list-66f51",
-    storageBucket: "to-do-list-66f51.firebasestorage.app",
-    messagingSenderId: "1035192080492",
-    appId: "1:1035192080492:web:6b6af8dc2fd07122cce049"
-};
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 // Initialize Cloud Firestore and get a reference to the service
